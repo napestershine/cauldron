e28c20317cf75f90a710acc10c63cac8f3129e1d
napestershine <napestershine@gmail.com>
2021-09-30 18:33:55 +0200

    added twig

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 class QuestionController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
@@ -19,8 +20,15 @@ class QuestionController extends AbstractController
     #[Route('/questions/{slug}', name: 'show')]
     public function show(string $slug): Response
     {
+        $answers = [
+            'Make sure your cat is sitting purrrfectly still 🤣',
+            'Honestly, I like furry shoes better than MY cat',
+            'Maybe... try saying the spell backwards?',
+        ];
+
         return $this->render('question/show.html.twig', [
-            'slug' => $slug,
+            'question' => ucwords(str_replace('-', ' ', $slug)),
+            'answers' => $answers
         ]);
     }
 }
